befcbd1761ab85df64187e5f05f83fa2dd603be9
Gustavo Maldonado <chiche@labu42.unix.lab>
2011-10-31 13:37:03 -0300

    scripts y alguna que otra correcion al informe

--- scripts/script.m
+++ scripts/script.m
@@ -5,7 +5,7 @@ desvioMin = 0.005;
 for j=1:50000,
     t=[0:0.02:13];
     x0=[78,13];
-    [t,x] = ode45(@func, t, x0);
+    [t,x] = ode45(@funcTriangular, t, x0);
     iteracion = j
     periodo = period(x(:,1),t) % con la funcion de gulle estimamos el periodo
 
